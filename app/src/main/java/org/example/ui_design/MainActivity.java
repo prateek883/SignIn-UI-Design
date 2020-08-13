26d38f1ddbccd6fcfaab81446ca76a8bedfb2e1f
Prateek Singh <singhprateek089@gmail.com>
2020-08-13 20:10:40 +0530

    UI Design

---
+++ app/src/main/java/org/example/ui_design/MainActivity.java
@@ -0,0 +1,21 @@
+package org.example.ui_design;
+
+import android.os.Build;
+import android.view.Window;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+    }
+}
